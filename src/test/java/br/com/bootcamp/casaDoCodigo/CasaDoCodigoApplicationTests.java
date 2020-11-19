fb7e8b19ebd388bf080f302ca4c8ef945b412ea2
Alexssander Fonseca <alexssander.fonseca@zup.com.br>
2020-11-19 09:42:00 -0300

    Setup do projeto

---
+++ src/test/java/br/com/bootcamp/casaDoCodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bootcamp.casaDoCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
